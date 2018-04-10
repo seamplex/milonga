3ffa93d2d374729aaf4f924b9691e4c724a0276d
gtheler <jeremy@seamplex.com>
2018-04-10 07:25:25 -0300

    updated year in version copyright

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 /*------------ -------------- -------- --- ----- ---   --       -            -
  *  milonga's version banner
  *
- *  Copyright (C) 2010--2016 jeremy theler
+ *  Copyright (C) 2010--2018 jeremy theler
  *
  *  This file is part of milonga.
  *
@@ -58,7 +58,7 @@ const char milongausage[] = "\
     MILONGA_SOLVER SPECTRUM smallest_eigenvalue ST_TYPE shift\n";
 
 const char milongacopyright[] = "\
- milonga is copyright (c) 2010-2016 jeremy theler\n\
+ milonga is copyright (c) 2010-2018 jeremy theler\n\
  licensed under GNU GPL version 3 or later.\n\
  milonga is free software: you are free to change and redistribute it.\n\
  There is NO WARRANTY, to the extent permitted by law.";
