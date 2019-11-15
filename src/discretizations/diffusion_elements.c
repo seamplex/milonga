seamplex/milonga

commit 934cdecebb3e8aef7e4c335d81d4aacb8deba410

fixed buggy check in bcs

src/discretizations/diffusion_elements.c
@@ -194,10 +194,10 @@ int diffusion_elements_matrices_build(void) {
       // pero aca miramos solo las de neumann y de robin porque las de dirichlet van
       // una vez que ensamblamos las matrizotas
       
-      if (wasora_mesh.main_mesh->element[i].physical_entity == NULL ||
-          wasora_mesh.main_mesh->element[i].physical_entity->bcs == NULL ||
-          wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_VACUUM ||
-          wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_UNDEFINED) {
+      if (wasora_mesh.main_mesh->element[i].physical_entity == NULL &&
+          wasora_mesh.main_mesh->element[i].physical_entity->bcs == NULL &&
+          (wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_VACUUM ||
+           wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_UNDEFINED)) {
           wasora_call(diffusion_elements_build_robin_objects(&wasora_mesh.main_mesh->element[i]));
       } else if (wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_MIRROR) {
           // TODO: que se puedan poner corrientes no nulas (no debe haber o fision o fuentes)

sha_wasora
@@ -1,1 +1,1 @@
-2a7ed527ac81585d553d47ac2fcd1b5e7fbcc698
+8e0019889801083992f248f353531e270dc08bea

src/discretizations/diffusion_volumes.c
@@ -283,8 +283,8 @@ int diffusion_volumes_matrices_build(void) {
             expr_t *bc_args = NULL;
               
             if (wasora_mesh.main_mesh->structured == 0) {
-              if (cell->neighbor[j].element != NULL ||
-                  cell->neighbor[j].element->physical_entity != NULL ||
+              if (cell->neighbor[j].element != NULL &&
+                  cell->neighbor[j].element->physical_entity != NULL &&
                   cell->neighbor[j].element->physical_entity->bcs != NULL) {
                 bc_type = cell->neighbor[j].element->physical_entity->bcs->type_phys;
                 bc_args = cell->neighbor[j].element->physical_entity->bcs->expr;

commit 8653c108ab04ad34f056efc828883fa034a309d3

reverted regression introduced in 934cdecebb

src/discretizations/diffusion_elements.c
@@ -194,10 +194,10 @@ int diffusion_elements_matrices_build(void) {
       // pero aca miramos solo las de neumann y de robin porque las de dirichlet van
       // una vez que ensamblamos las matrizotas
       
-      if (wasora_mesh.main_mesh->element[i].physical_entity == NULL &&
-          wasora_mesh.main_mesh->element[i].physical_entity->bcs == NULL &&
-          (wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_VACUUM ||
-           wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_UNDEFINED)) {
+      if (wasora_mesh.main_mesh->element[i].physical_entity == NULL ||
+          wasora_mesh.main_mesh->element[i].physical_entity->bcs == NULL ||
+          wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_VACUUM ||
+          wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_UNDEFINED) {
           wasora_call(diffusion_elements_build_robin_objects(&wasora_mesh.main_mesh->element[i]));
       } else if (wasora_mesh.main_mesh->element[i].physical_entity->bcs->type_phys == BC_MIRROR) {
           // TODO: que se puedan poner corrientes no nulas (no debe haber o fision o fuentes)